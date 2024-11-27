66876a7cc26d76502f8082df08ef440e7b4d7095
Ram Nihar Kaligatla <bhavyagunaram@gmail.com>
2024-11-27 16:08:56 +0530

    Add files via upload

---
+++ CubeOfNumber.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main()
+{
+	float a;
+	scanf("%f",&a);
+	printf("Cube of number is %f",a*a*a);
+	return 0;
+}
+
---
+++ PercentageOfStudents.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main()
+{
+	int m1,m2,m3,m4,m5;
+	scanf("%d %d %d %d %d",&m1,&m2,&m3,&m4,&m5);
+	printf("Percentage of student is %f",(float)((m1+m2+m3+m4+m5)/5));
+	return 0;
+}
